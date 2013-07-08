513cbbb06679d9a3ddb5fcc407887d1f6f61ef5a
ATR <ironapple@gmail.com>
2013-07-08 01:08:01 -0400

    First test of the routing

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('What\'s up' );
+});
+
+app.get('/message', function(request, response) {
+  response.send('This is my message' );
 });
 
 var port = process.env.PORT || 5000;
