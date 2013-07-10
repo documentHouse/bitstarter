e419cfcb0ba1fe83f509f9a44282fbe4ccb98739
AR <ironapple@gmail.com>
2013-07-10 02:26:49 +0000

    Simple code clean up

--- web.js
+++ web.js
@@ -1,22 +1,24 @@
+"use strict";
 var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  var indexBuffer = fs.readFileSync('index.html');
-  response.send(indexBuffer.toString());
+var indexBuffer = fs.readFileSync('index.html');
+
+app.get('/', function (request, response) {
+    response.send(indexBuffer.toString());
 });
 
-app.get('/message', function(request, response) {
-  response.send('This is my message' );
+app.get('/message', function (request, response) {
+    response.send('This is my message');
 });
 
-app.get('/info', function(request, response) {
-  response.send('<html><head></head><body><h1>Bold, but no info</h1></body></html>' );
+app.get('/info', function (request, response) {
+    response.send('<html><head></head><body><h1>Bold, but no info</h1></body></html>');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
